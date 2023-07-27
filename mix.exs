1e3294ea007389919bb6f1ddd6f8311bde5b4563
Alex Bruns <alex.h.bruns@gmail.com>
2023-07-27 01:39:25 -0500

    bump minor version
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule LiveQueryClientLiveView.MixProject do
       docs: [
         main: "LiveQuery.Clients.LiveView"
       ],
-      version: "0.1.1",
+      version: "0.1.2",
       source_url: "https://github.com/AHBruns/live_query_client_live_view",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
